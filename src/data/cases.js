794122bce6a0f1d1117691c18c4adc7d514f762e
Nicolás González <nicolook.pro@gmail.com>
2024-03-23 11:08:53 -0300

    start

---
+++ src/data/cases.js
@@ -0,0 +1,42 @@
+const CASES = [
+  
+    
+        {
+            "id": 1,
+            "title": "Stadium Sports",
+            "subtitle": "Preciadores <br> Tipografía pixelar",
+            "desc": "Desarrollada en I+D como parte del proyecto de Stadium Sport, aplicada para los predicadores del local.",
+            "img": [
+              "img/1920x1080.svg",
+              "img/1920x1080.svg",
+              "img/1920x1080.svg",
+            ],
+        },
+
+        {
+            "id": 2,
+            "title": "Caso 2",
+            "subtitle": "Mauris turpis nunc blandit et",
+            "desc": "Nunc nec neque. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Etiam ut purus mattis mauris sodales aliquam.",
+            "img": [
+              "img/1920x1080.svg",
+              "img/1920x1080.svg",
+              "img/1920x1080.svg",
+            ],
+        },
+
+        {
+            "id": 3,
+            "title": "Caso 3",
+            "subtitle": "Mauris turpis nunc blandit et",
+            "desc": "Nunc nec neque. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Etiam ut purus mattis mauris sodales aliquam.",
+            "img": [
+              "img/1920x1080.svg",
+              "img/1920x1080.svg",
+              "img/1920x1080.svg",
+            ],
+        },
+       
+    ]
+
+export { CASES }
---
+++ src/data/slides.js
@@ -0,0 +1,29 @@
+const SLIDES = [
+    
+        {
+            "id": 1,
+            "title": "Stadium Sports",
+            "subtitle": "Preciadores <br> Tipografía pixelar",
+            "desc": "Desarrollada en I+D como parte del proyecto de Stadium Sport, aplicada para los predicadores del local.",
+            "img": "img/1920x1080.svg",
+        },
+
+        {
+            "id": 2,
+            "title": "Aenean commodo ligula eget dolor",
+            "subtitle": "Mauris turpis nunc blandit et",
+            "desc": "Nunc nec neque. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Etiam ut purus mattis mauris sodales aliquam.",
+            "img": "img/1920x1080.svg",
+        },
+
+        {
+            "id": 3,
+            "title": "Aenean commodo ligula eget dolor",
+            "subtitle": "Mauris turpis nunc blandit et",
+            "desc": "Nunc nec neque. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Etiam ut purus mattis mauris sodales aliquam.",
+            "img": "img/1920x1080.svg",
+        },
+       
+    ]
+
+export { SLIDES }
