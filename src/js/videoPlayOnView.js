f0d2e28424911332f7f3c25e26285d74c11be95a
Nicolás González <nicolook.pro@gmail.com>
2024-03-26 08:31:05 -0300

    Video Load & Play

--- src/js/videoPlayOnView.js
+++ src/js/videoPlayOnView.js
@@ -16,7 +16,13 @@ function videoPlayOnView() {
       // Si el video está completamente visible, reproducirlo
       if (entry.isIntersecting) {
         const video = entry.target;
+
+        if (video.readyState !== 4) {
+            video.load();
+        }
+
         video.play();
+        
       } else {
         // Pausar el video si no está visible
         const video = entry.target;
